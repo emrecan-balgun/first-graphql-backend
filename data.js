4ee1bdef4df14d754926f2c9c4249827a866171a
Emrecan Balgün <deathlygrave@gmail.com>
2022-03-15 15:45:28 +0300

    update data.js

--- data.js
+++ data.js
@@ -3,22 +3,7 @@ const authors = [{
     name: "Albert",
     surname: "Camus",
     age: "41",
-    books: [
-        {
-        id: "1",
-        title: "The Stranger",
-        score: "9",
-        isPublished: false
     },
-    {
-        id: "2",
-        title: "The Mother",
-        score: "7",
-        isPublished: true
-    },
-    ]
-    },
-
     {
         id: "2",
         name: "Robert",
@@ -32,14 +17,14 @@ const books = [
     {
         id: "1",
         title: "Yabancı",
-        author: authors[0],
+        author_id: "1",
         score: 3.4,
         isPublished: true,
     },
     {
         id: "2",
         title: "Hırsız",
-        author: authors[0],
+        author_id: "2",
         score: 7.1,
         isPublished: false,
     },
